154a26d1b0ce1acf151eecb62313fe2936e9112e
ICEY <1595932574@qq.com>
2023-02-14 08:22:12 +0800

    Update Process_Thread.c

--- KernelSpecialAPC_ReadProcessMemory/Process_Thread.c
+++ KernelSpecialAPC_ReadProcessMemory/Process_Thread.c
@@ -6,7 +6,7 @@ BOOLEAN Get_pEprocess_By_Pid(ULONG64 Pid, PEPROCESS* pProcess) {
 	PLIST_ENTRY64 P_ProcessListEntry = (PLIST_ENTRY64)((ULONG64)CurrentEprocess+ ProcessListEntry);
 	PLIST_ENTRY64 LS = P_ProcessListEntry->Flink;
 	PEPROCESS targetProcess = NULL;
-	ULONG64 num = 0;//要是找了500个进程都找不到，就返回错误。（或者查询链表时其他线程有对链表的操作）
+	ULONG64 num = 0;//瑕佹槸鎵句簡500涓繘绋嬮兘鎵句笉鍒帮紝灏辫繑鍥為敊璇�傦紙鎴栬�呮煡璇㈤摼琛ㄦ椂鍏朵粬绾跨▼鏈夊閾捐〃鐨勬搷浣滐級
 	BOOLEAN Find = FALSE;
 	__try {
 		while (LS != P_ProcessListEntry && num <500)
@@ -26,7 +26,7 @@ BOOLEAN Get_pEprocess_By_Pid(ULONG64 Pid, PEPROCESS* pProcess) {
 	return Find;
 }
 
-BOOLEAN Get_APC_pThrean_By_Process(PEPROCESS Process, PETHREAD* pThread) {
+BOOLEAN Get_APC_pThread_By_Process(PEPROCESS Process, PETHREAD* pThread) {
 	PETHREAD targetThrean = NULL;
 	PLIST_ENTRY64 P_ThreadListHead = (PLIST_ENTRY64)((ULONG64)Process + ThreadListHead);
 	PLIST_ENTRY64 LS = P_ThreadListHead->Blink;
@@ -36,7 +36,7 @@ BOOLEAN Get_APC_pThrean_By_Process(PEPROCESS Process, PETHREAD* pThread) {
 		{
 			targetThrean = (ULONG64)LS - ThreadListEntry;
 			if (*(PUCHAR)((ULONG64)targetThrean + Running) == TRUE && *(PUSHORT)((ULONG64)targetThrean + SpecialApcDisable) == 0) {
-				//线程正在运行 并且 未关闭APC
+				//绾跨▼姝ｅ湪杩愯 骞朵笖 鏈叧闂瑼PC
 				*pThread = targetThrean;
 				Find = TRUE;
 				break;
